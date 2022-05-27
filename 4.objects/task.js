96990c71f2eb91d464fcc061a9c9f9a9c55c885a
Belova <katishen@mail.ru>
2022-05-27 16:21:34 +0500

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  let marks = this.marks;
+
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...manyMarks) {
+  let marks = this.marks;
+
+  if (this.marks === undefined) {
+    this.marks = manyMarks;
+  } else {
+    manyMarks.forEach((num) => {
+      this.marks.push(num)
+    })
+  }
+};
+
+Student.prototype.getAverage = function () {
+  let summa = (this.marks).reduce((sum, elem) => {
+    return sum + elem
+  }, 0);
+
+  return medium = summa / (this.marks).length
+};
+
+Student.prototype.exclude = function (reason) {
+  if (this.subject && this.marks) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason
+  }
+}
