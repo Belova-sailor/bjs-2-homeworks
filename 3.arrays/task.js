464cc03d5918ab9ebab5d854bec9ecddbd162744
Belova <katishen@mail.ru>
2022-05-27 13:35:36 +0500

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,27 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length
+    && arr1.every((value, index) => value === arr2[index])) {
+    result = true
+  } else {
+    result = false
+  }
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let getPositiveNumber = (item) => item > 0;
 
-  // Ваш код
+  function getMultiplesThree(item) {
+    const remainder = item % 3;
+    return remainder === 0
+  };
+
+  let getMultiplesTen = (remainder) => remainder = remainder * 10;
+
+  let resultArr = arr.filter(getPositiveNumber).filter(getMultiplesThree).map(getMultiplesTen);
 
   return resultArr; // array
 }
